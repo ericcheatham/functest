1ae41c578c60f37f76734cd1715e7df4ade7f5b8
Eric Cheatham <eric@meroxa.io>
2023-08-07 11:31:07 -0400

    test

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ class App:
             # with the `resources` function.
             # Replace `source_name` with the resource name the
             # data store was configured with on the Meroxa platform.
-            # source = await turbine.resources("notion")
+            source = await turbine.resources("notion")
 
             # Specify which upstream records to pull
             # with the `records` function.
@@ -32,7 +32,7 @@ class App:
             # or bucket name in your data store.
             # If you need additional connector configurations, replace '{}'
             # with the key and value, i.e. {"incrementing.field.name": "id"}
-            # records = await source.records("*", {})
+            records = await source.records("*", {})
 
             # Specify which secrets in environment variables should be passed
             # into the Process.
@@ -49,15 +49,10 @@ class App:
             # with the `resources` function.
             # Replace `destination_name` with the resource name the
             # data store was configured with on the Meroxa platform.
-            destination_db = await turbine.resources("url")
-            print('hi')
-            # Specify where to write records downstream
-            # using the `write` function.
-            # Replace `collection_archive` with a table, collection,
-            # or bucket name in your data store.
-            # If you need additional connector configurations, replace '{}'
-            # with the key and value, i.e. {"behavior.on.null.values": "ignore"}
-            rec = Records(records=RecordList(), stream="streamtest", name="nametest")
-            await destination_db.write(rec , "collection_archive", {})
+            # destination_db = await turbine.resources("url")
+            # print('hi')
+  
+            # rec = Records(records=RecordList(), stream="streamtest", name="nametest")
+            # await destination_db.write(rec , "collection_archive", {})
         except Exception as e:
             print(e, file=sys.stderr)
