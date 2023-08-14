593149ebf2f0c5594512c1b2d635e6241f98e322
Eric Cheatham <eric@meroxa.io>
2023-08-14 13:53:09 -0400

    test

--- main.py
+++ main.py
@@ -22,22 +22,9 @@ class App:
 
             records = await source.records("*", {})
 
-            # Specify which secrets in environment variables should be passed
-            # into the Process.
-            # Replace 'PWD' with the name of the environment variable.
-            #
-            # turbine.register_secrets("PWD")
-
-            # Specify what code to execute against upstream records
-            # with the `process` function.
-            # Replace `anonymize` with the name of your function code.
             # anonymized = await turbine.process(records, anonymize)
 
-            # Identify a downstream data store for your data app
-            # with the `resources` function.
-            # Replace `destination_name` with the resource name the
-            # data store was configured with on the Meroxa platform.
-            destination_db = await turbine.resources("cheatham-s3")        
+            destination_db = await turbine.resources("s3")        
             await destination_db.write(records , "collection_archive", {})
         except Exception as e:
             print(e, file=sys.stderr)
